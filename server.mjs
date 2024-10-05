c259240fc3aae0b39686b131a32b961ddd578610
sajjadahmad18108 <sajjadahmad18108@gmail.com>
2024-10-05 13:30:38 +0500

    solve error

--- server.mjs
+++ server.mjs
@@ -3,6 +3,9 @@ import express from "express"
 const app = express()
 const PORT = 3000
 
+app.get('/', (req, res) => {
+    res.send("This is the Home");
+});
 app.get('/user', (req, res) => {
     res.send("This is the Home page");
 });
